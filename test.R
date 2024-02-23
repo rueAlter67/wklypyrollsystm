33bcd37b3e665b59cbdf7bf08b06a27f8544b68a
Chrono-123 <145533807+Chrono-123@users.noreply.github.com>
2024-02-23 23:51:44 +0800

    Update test.R

--- test.R
+++ test.R
@@ -3,3 +3,6 @@ employee <- setClass("employee", slots = list(workedDays = "numOfDays", workedHo
 computeSalary <- function(){
   #function for computing salary
   }
+employeeSalary = new ("employee", numOfDays, workOfDays
+computeSalary()https://www.instagram.com/reel/C02DzCoIbim/?utm_source=ig_web_copy_link
+
