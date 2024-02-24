472e9f33e0214fab35b90821f454d733b5b1404b
Chrono-123 <145533807+Chrono-123@users.noreply.github.com>
2024-02-24 22:26:13 +0800

    Update test.R

--- test.R
+++ test.R
@@ -4,5 +4,5 @@ computeSalary <- function(){
   #function for computing salary
   }
 employeeSalary = new ("employee", numOfDays, workOfDays
-computeSalary()https://www.instagram.com/reel/C02DzCoIbim/?utm_source=ig_web_copy_link
+computeSalary()
 
