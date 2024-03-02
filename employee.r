eb88b8a50f98ac87e294cd6d78aebb345ce83443
Chrono-123 <145533807+Chrono-123@users.noreply.github.com>
2024-03-03 01:18:22 +0800

    Update and rename test.R to employee.r

--- employee.r
+++ employee.r
@@ -1,8 +1,4 @@
 employee <- setClass("employee", slots = list(workedDays = "numOfDays", workedHours = "numOfHours", toBePaid = "salary")) #employee class/object
 
-computeSalary <- function(){
-  #function for computing salary
-  }
-employeeSalary = new ("employee", numOfDays, workOfDays
-computeSalary()
+
 
