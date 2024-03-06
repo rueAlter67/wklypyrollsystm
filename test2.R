95e1f1cfedf0938d3185a894fc3c6adc35ce197b
rueAlter67 <emma_cacatian@dlsu.edu.ph>
2024-03-07 06:57:49 +0800

    added date classifier for holidays

--- test2.R
+++ test2.R
@@ -1,2 +1,29 @@
 employee <- setClass("employee", slots = list(workedDays = "numeric", workedHours = "numeric", toBePaid = "numeric")) #employee class/object
 
+
+running = TRUE
+while(running){
+  print("Weekly Payroll System")
+  print("[1] Calculate weekly salary")
+  print("[2] Quit application")
+  
+  choice <- readline("Enter choice: ")
+  base_date <- ymd("20240101")
+  if(choice==1 ){
+    year <- 2024
+    month <- get_month()
+    
+    day <-  get_day(month)
+    
+    current_date <- ymd(paste(year,month,day))
+    
+    
+    invisible(print(paste("The date is",current_date)))
+    
+  }else{
+    running <- FALSE
+  }
+}
+
+print("You've quit the application")
+
