65f6546718ef5a8a31baae5d86b8682effd88d0c
Chrono-123 <145533807+Chrono-123@users.noreply.github.com>
2024-03-12 22:53:03 +0800

    Delete employee.R

--- employee.R
+++
@@ -1,4 +0,0 @@
-employee <- setRefClass("employee", slots = list(timeIn = "clockIn", timeOut = "clockOut", dayType = "typeOfDay", overTime = "numOfOver", nightShift = "dayOrNight")) #employee class/object
-
-
-
