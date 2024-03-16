e91bfc98c7ea840c7048faef9b63c24c92e1d1a6
Chrono-123 <145533807+Chrono-123@users.noreply.github.com>
2024-03-16 11:04:37 +0800

    Update tryingSomething.R

--- tryingSomething.R
+++ tryingSomething.R
@@ -6,7 +6,10 @@ library(lubridate)
 special_dates <- read_csv('dates.csv')
 
 computeOverTime <- function(timeIn, timeOut){
-    return over <- timeOut - timeIn
+    if (timeIn < timeOut){
+        timeAfterMidnight <- (2400 - timeIn)
+        
+      }
   }
 salaryCompute <- function(timeIn, timeOut, dayOfWeek, workDayType){
     overTime <- computeOverTime(timeIn, timeOut)
@@ -20,8 +23,8 @@ salaryCompute <- function(timeIn, timeOut, dayOfWeek, workDayType){
     
     choice <- readline("Enter choice: ")
     if(choice==1 ){
-      timeIn <- readLine("Time In: ")
-      timeOut <- readLine("Time Out: ")
+      timeIn <- readLine("Time In (input in military time [ie. 1000 for 10am, 2300, for 11pm, 1430 for 2:30pm]): ")
+      timeOut <- readLine("Time Out (input in military time [ie. 1000 for 10am, 2300, for 11pm, 1430 for 2:30pm]): ")
       dayOfWeek <- readLine("Day: ")
       if(dayOfWeek == "Saturday" || dayOfWeek == "Sunday"){
         workDayType <- "Rest Day"
