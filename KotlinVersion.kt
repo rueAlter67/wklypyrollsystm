05b2d1eae37df99df65236c99bfc777a4e9e30a1
Chrono-123 <145533807+Chrono-123@users.noreply.github.com>
2024-03-29 18:59:27 +0800

    Update KotlinVersion.kt

--- KotlinVersion.kt
+++ KotlinVersion.kt
@@ -1,6 +1,45 @@
+val reader = Scanner(System.`in`)
+
+fun compute(shift: String, timeDiff: Int){
+    val dailySalary = 500
+    if (shift == "Night Shift"){
+        var totalSalary = dailySalary + ((dailySalary * 1.375) * timeDiff)
+    }
+    else{
+        var totalSalary = dailySalary
+    }
+    return totalSalary
+}
+fun salary(){
+    var shiftInWords
+    print("Time In: ")
+    val timeIn = reader.nextInt()
+    print("Time Out: ")
+    val timeOut = reader.nextInt()
+    println("Choose Shift")
+    println("[1] Day Shift")
+    println("[2] Night Shift")
+    val shift = reader.nextInt()
+
+    switch(shift){
+        case 1: shiftInWords = "Day Shift"
+        break;
+        case 2: shiftInWords = "Night Shift"
+    }
+    val timeDiff = timeOut - 2100
+    var salaryCompute = compute(shiftInWords, timeDiff)
+    return salaryCompute
+}
 fun main(){
-   val timeIn = 0900
-   val timeOut = 2300
-   val overTime = timeOut - 2100
-   val nightShift = false
+    
+    println("Weekly Payroll System")
+    println("[1] Calculate Weekly Salary")
+    println("[2] Quit Application")
+    val choice = reader.nextInt()
+
+    if(choice == 1){
+        var Salary = salary()
+        println("Salary: $Salary")
+    }
+
 }
