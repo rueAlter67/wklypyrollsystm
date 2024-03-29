9c62665d9e608d12f046dd7a862bdc6139020063
Chrono-123 <145533807+Chrono-123@users.noreply.github.com>
2024-03-29 19:44:26 +0800

    Update KotlinVersion.kt

--- KotlinVersion.kt
+++ KotlinVersion.kt
@@ -1,31 +1,27 @@
+import java.util.Scanner
 val reader = Scanner(System.`in`)
 
-fun compute(shift: String, timeDiff: Int){
-    val dailySalary = 500
+fun compute(shift: String, timeDiff: Int): Double{
+    val dailySalary: Double = 500.00
+    var totalSalary = dailySalary
     if (shift == "Night Shift"){
-        var totalSalary = dailySalary + ((dailySalary * 1.375) * timeDiff)
-    }
-    else{
-        var totalSalary = dailySalary
+        totalSalary = dailySalary + ((dailySalary * 1.375) * timeDiff)
     }
     return totalSalary
 }
-fun salary(){
-    var shiftInWords
-    print("Time In: ")
-    val timeIn = reader.nextInt()
+fun salary(): Double{
+    var shiftInWords = "Night Shift"
     print("Time Out: ")
-    val timeOut = reader.nextInt()
+    val timeOut: Int = reader.nextInt()
     println("Choose Shift")
     println("[1] Day Shift")
     println("[2] Night Shift")
-    val shift = reader.nextInt()
+    val shift: Int = reader.nextInt()
 
-    switch(shift){
-        case 1: shiftInWords = "Day Shift"
-        break;
-        case 2: shiftInWords = "Night Shift"
+    if (shift == 1){
+        shiftInWords = "Day Shift"
     }
+ 
     val timeDiff = timeOut - 2100
     var salaryCompute = compute(shiftInWords, timeDiff)
     return salaryCompute
@@ -35,7 +31,7 @@ fun main(){
     println("Weekly Payroll System")
     println("[1] Calculate Weekly Salary")
     println("[2] Quit Application")
-    val choice = reader.nextInt()
+    val choice: Int = reader.nextInt()
 
     if(choice == 1){
         var Salary = salary()
